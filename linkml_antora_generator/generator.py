0c9b35ce6ffd812c4c78832818d10e2cde0aa12d
Bart Kleijngeld <bartkl@gmail.com>
2024-12-03 16:56:08 +0100

    WIP

--- linkml_antora_generator/generator.py
+++ linkml_antora_generator/generator.py
@@ -2,7 +2,7 @@ from enum import Enum, auto
 import jinja2
 import subprocess
 import textwrap
-from os import PathLike
+import os
 import os.path
 from collections.abc import Callable
 import linkml_antora_generator.schema as Schema
@@ -12,6 +12,8 @@ from xml.etree import ElementTree
 from urllib.parse import urlparse
 from functools import reduce
 from pprint import pprint
+import shutil
+import yaml
 
 from linkml_antora_generator.linkml_full import (
     SchemaDefinition,
@@ -23,7 +25,9 @@ from linkml_antora_generator.linkml_full import (
 type ClassName = str
 type EnumName = str
 type AsciiDocStr = str
+type YamlDocStr = str
 type AntoraResourceId = str
+type AntoraYmlDict = dict  # TODO: Make `TypedDict`.
 type MermaidCode = str
 type SvgImageStr = str
 type CURIE = str
@@ -31,23 +35,9 @@ type CURIE = str
 
 JINJA2_TEMPLATES_DIR = "templates"
 ANTORA_COMPONENT_VERSION_DIR = "output"
-ANTORA_ROOT_MODULE = os.path.join(
-    ANTORA_COMPONENT_VERSION_DIR, "modules", "ROOT"
-)
 MERMAID_MMDC = (
     "/home/bart/Programming/Netbeheer-Nederland/docs/node_modules/.bin/mmdc"
 )
-
-
-def cim_class_color(class_: ClassDefinition) -> str:
-    package = str(urlparse(class_.from_schema).fragment)
-    top_level_package = ".".join(package.split(".")[:2])
-
-    return {
-        "TC57CIM.IEC61970": "rgb(236, 207, 203)",
-        "TC57CIM.IEC61968": "rgb(209, 231, 194)",
-        "TC57CIM.IEC62325": "rgb(255, 251, 239)",
-    }[top_level_package]
 
 
 def default_class_color(class_: ClassDefinition) -> str:
@@ -63,19 +53,24 @@ class ResourceType(Enum):
 
 class AntoraDocsGenerator:
     template_map = {ResourceType.CLASS_PAGE: "class.adoc.jinja2", ResourceType.ENUM_PAGE: "enum.adoc.jinja2"}
-    diagram_background_color = "#ffffff"
 
     def __init__(
         self,
-        schema: SchemaDefinition,
+        schemas: set[SchemaDefinition],
         class_color: Callable[[ClassDefinition], str] | None = None,
+        output_dir: os.Pathlike | None = None,
+        metadata: AntoraYmlDict | None = None,
+        diagram_background_color: str | None = None,
     ) -> None:
-        self.schema = schema
+        self.schemas = set(schemas)
+        self.class_color = class_color or default_class_color
+        self.output_dir = output_dir or ANTORA_COMPONENT_VERSION_DIR
+        self.metadata = metadata
+        self.diagram_background_color = diagram_background_color or "#ffffff"
+
         self.jinja2_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(JINJA2_TEMPLATES_DIR)
         )
-        self.class_color = class_color or default_class_color
-        self.output_dir = ANTORA_ROOT_MODULE
 
     def _render_template(
         self,
@@ -141,9 +136,9 @@ class AntoraDocsGenerator:
             diagram_code += f'\t{class_name} --> "{slot_cardinalities}" {slot.range}: {slot_name}\n'
         return diagram_code
 
-    def class_relations_diagram(self, class_: ClassDefinition) -> SvgImageStr:
+    def class_relations_diagram(self, schema: SchemaDefinition, class_: ClassDefinition) -> SvgImageStr:
         class_name = class_._meta["name"]
-        relation_slots = Schema.relations(class_, self.schema)
+        relation_slots = Schema.relations(class_, schema)
         diagram_res_id = self._resource_id(
             ResourceType.CLASS_RELATIONS_DIAGRAM, class_name
         )
@@ -164,6 +159,7 @@ class AntoraDocsGenerator:
 
     def _colored_class_relations_diagram(
         self,
+        schema: SchemaDefinition,
         diagram_svg: SvgImageStr,
         diagram_name: str,
     ) -> SvgImageStr:
@@ -184,7 +180,7 @@ class AntoraDocsGenerator:
                 f".//{{{html_ns}}}span/{{{html_ns}}}p"
             )
             try:
-                color = self.class_color(self.schema.classes[class_label])
+                color = self.class_color(schema.classes[class_label])
             except KeyError:
                 color = "#cccccc"
 
@@ -194,26 +190,6 @@ class AntoraDocsGenerator:
         colored_diagram_svg = ElementTree.tostring(svg_root)
 
         return colored_diagram_svg
-
-    def create_class_page(self, class_: ClassDefinition) -> None:
-        class_name = class_._meta["name"]
-
-        relation_slots = Schema.relations(class_, self.schema)
-        relations_diagram_res_id = None
-        if relation_slots:
-            relations_diagram: SvgImageStr = self.class_relations_diagram(
-                class_
-            )
-            relations_diagram_res_id = self._resource_id(
-                ResourceType.CLASS_RELATIONS_DIAGRAM, class_name
-            )
-            self._write_file(relations_diagram, relations_diagram_res_id)
-
-        class_page: AsciiDocStr = self._class_page(
-            class_, relations_diagram_res_id
-        )
-        class_res_id = self._resource_id(ResourceType.CLASS_PAGE, class_name)
-        self._write_file(class_page.encode("utf-8"), class_res_id)
 
 
     def _enum_page(self, enum: EnumDefinition) -> AsciiDocStr:
@@ -228,22 +204,14 @@ class AntoraDocsGenerator:
         )
 
 
-    def create_enum_page(self, enum: EnumDefinition) -> None:
-        enum_name = enum._meta["name"]
-
-        enum_page: AsciiDocStr = self._enum_page(enum)
-        enum_res_id = self._resource_id(ResourceType.ENUM_PAGE, enum_name)
-        self._write_file(enum_page.encode("utf-8"), enum_res_id)
-
-
     def _class_hierarchy(self, class_: ClassDefinition) -> AsciiDocStr:
         return self._class_ancestors(class_) + self._class_descendants(class_)
 
 
-    def _class_ancestors(self, class_: ClassDefinition) -> AsciiDocStr:
+    def _class_ancestors(self, schema: SchemaDefinition, class_: ClassDefinition) -> AsciiDocStr:
         # Ancestors.
         ancestor_classes = [
-            c._meta["name"] for c in Schema.get_ancestors(class_, self.schema)
+            c._meta["name"] for c in Schema.get_ancestors(class_, schema)
         ][::-1]
         hierarchy_adoc = reduce(
             lambda acc, succ: f"{acc}{'*'*succ[0]} {self._class_xref(succ[1])}\n",
@@ -258,13 +226,13 @@ class AntoraDocsGenerator:
 
         return hierarchy_adoc
 
-    def _class_descendants(self, class_: ClassDefinition) -> AsciiDocStr:
+    def _class_descendants(self, schema: SchemaDefinition, class_: ClassDefinition) -> AsciiDocStr:
         # Descendants.
 
         subclasses = sorted(
             [
                 c._meta["name"]
-                for c in Schema.get_subclasses(class_, self.schema)
+                for c in Schema.get_subclasses(class_, schema)
             ]
         )[:3]
         hierarchy_adoc = reduce(
@@ -285,14 +253,15 @@ class AntoraDocsGenerator:
         return f"xref::{enum_res_id}[`{enum_name}`]"
 
 
-    def _link_curie(self, curie: CURIE) -> AsciiDocStr:
+    def _link_curie(self, schema: SchemaDefinition, curie: CURIE) -> AsciiDocStr:
         prefix, ncname = curie.split(":")
-        base_uri = self.schema.prefixes[prefix]
+        base_uri = schema.prefixes[prefix]
         return f"{base_uri}{ncname}[`{curie}`]"
 
 
     def _class_page(
         self,
+        schema: SchemaDefinition,
         class_: ClassDefinition,
         relations_diagram_res_id: AntoraResourceId | None,
     ) -> AsciiDocStr:
@@ -304,9 +273,9 @@ class AntoraDocsGenerator:
             class_name=class_name,
             class_type=self.class_type(class_),
             class_hierarchy=self._class_hierarchy(class_),
-            relations=Schema.relations(class_, self.schema),
+            relations=Schema.relations(class_, schema),
             render_images=True,
-            attributes=Schema.attributes(class_, self.schema),
+            attributes=Schema.attributes(class_, schema),
             cardinalities=self._cardinalities,
             class_xref=self._class_xref,
             link_curie=self._link_curie,
@@ -346,8 +315,8 @@ class AntoraDocsGenerator:
         return svg_image
 
     def _write_file(
-        self, content: bytes, resource_id: AntoraResourceId
-    ) -> PathLike:
+            self, schema: SchemaDefinition, content: bytes, resource_id: AntoraResourceId
+    ) -> os.PathLike:
         # TODO: Improve.
         if resource_id.endswith(".svg"):
             family = "images"
@@ -356,42 +325,127 @@ class AntoraDocsGenerator:
         else:
             family = "pages"
 
-        filepath = os.path.join(self.output_dir, family, resource_id)
+        filepath = os.path.join(self.output_dir, schema.id, family, resource_id)
         with open(filepath, mode="wb") as f:
             f.write(content)
 
         return filepath
 
-    def _nav_page(self) -> AsciiDocStr:
+    def _nav_page(self, schema: SchemaDefinition) -> AsciiDocStr:
         nav = ".Classes\n"
-        for section_letter, classes_group in groupby(sorted(self.schema.classes), key=itemgetter(0)):
+        for section_letter, classes_group in groupby(sorted(schema.classes), key=itemgetter(0)):
             nav += f"* {section_letter}\n"
             for class_ in classes_group:
                 nav += f"** {self._class_xref(class_, mono=False)}\n"
 
         nav += ".Enumerations\n"
-        for enum in self.schema.enums:
+        for enum in schema.enums:
             nav += f"* {self._enum_xref(enum)}\n"
 
         #nav = reduce(
         #    lambda acc, succ: f"{acc}* {self._class_xref(succ)}\n",
-        #self.schema.classes.keys(),
+        #schema.classes.keys(),
         #"",
         #)
         return nav
 
+    def _component_version_descriptor(self, metadata: AntoraYmlDict) -> YamlDocStr:
+        if "version" not in metadata:
+            metadata["version"] = True
 
-    def create_nav_page(self) -> None:
+        antora_yml = yaml.safe_dump(antora_yml_dict)
+
+        return antora_yml
+
+    # ~~~ Methods with side effects.
+
+    def _create_component_version_descriptor(self) -> os.PathLike:
+        antora_yml = self._component_version_descriptor(self.metadata)
+        self._write_file(antora_yml.encode("utf-8"))  # TODO
+
+
+    def _create_nav_page(self) -> None:
         nav_page: AsciiDocStr = self._nav_page()
         nav_res_id = self._resource_id(ResourceType.NAV_PAGE)
         self._write_file(nav_page.encode("utf-8"), nav_res_id)
 
+    def _create_enum_page(self, schema: SchemaDefinition, enum_name: EnumName) -> None:
+        enum = schema.enums[enum_name]
+
+        enum_page: AsciiDocStr = self._enum_page(enum)
+        enum_res_id = self._resource_id(ResourceType.ENUM_PAGE, enum_name)
+        self._write_file(enum_page.encode("utf-8"), enum_res_id)
+
+    def _create_class_page(self, schema: SchemaDefinition, class_name: ClassName, incl_relations_diagram: bool = False) -> None:
+        class_= schema.classes[class_name]
+
+        relation_slots = Schema.relations(class_, schema)
+
+        relations_diagram_res_id = None
+        if relation_slots:
+            relations_diagram: SvgImageStr = self.class_relations_diagram(
+                class_
+            )
+            relations_diagram_res_id = self._resource_id(
+                ResourceType.CLASS_RELATIONS_DIAGRAM, class_name
+            )
+            self._write_file(relations_diagram, relations_diagram_res_id)
+
+        class_page: AsciiDocStr = self._class_page(
+            class_, relations_diagram_res_id
+        )
+        class_res_id = self._resource_id(ResourceType.CLASS_PAGE, class_name)
+        self._write_file(class_page.encode("utf-8"), class_res_id)
+
+    def _module_dir(self, name: str) -> os.PathLike:
+        return os.path.join(self.output_dir, "modules", name)
+
+    def _create_module_dir(self, name: str) -> os.PathLike:
+        dir_path = self._module_dir(name)
+        os.makedirs(dir_path, exist_ok=True)
+
+        return dir_path
+
+    def _create_module(self, schema: SchemaDefinition, name: str | None = None) -> None:
+        if not name:
+            name = schema.id
+
+        module_dir = self._create_module_dir(name)
+
+        for class_name in schema.classes:
+            self._create_class_page(schema, class_name)
+
+        for enum_name in schema.enums:
+            self._create_enum_page(schema, enum_name)
+
+        #for type_name in schema.types:
+        #    self._create_type_page(schema, enum_name)
+
+        self._create_nav_page()
+
+    def _create_output_dir(self) -> None:
+        os.makedirs(self.output_dir, exist_ok=True)
+
+    def _clean_output_dir(self) -> None:
+        if os.path.exists(self.output_dir):
+            shutil.rmtree(self.output_dir)
+
+    def _init_output_dir(self) -> None:
+        self._clean_output_dir()
+        self._create_output_dir()
+
+    def _create_component_version(self) -> None:
+        self._init_output_dir()
+        self._create_component_version_descriptor()
+
+        if len(self.schemas) == 1:
+            self._create_module(self.schemas[0], name="ROOT")
+        else:
+            for schema in self.schemas:
+                self._create_module(schema)
 
     def create_docs(self) -> None:
-        for class_ in self.schema.classes.values():
-            self.create_class_page(class_)
+        """Entry point.
+        """
 
-        for enum in self.schema.enums.values():
-            self.create_enum_page(enum)
-
-        self.create_nav_page()
+        self._create_component_version()
--- linkml_antora_generator/main.py
+++ linkml_antora_generator/main.py
@@ -2,18 +2,33 @@ import linkml_prof_logical_model as linkml_lang
 import yaml
 from jinja2 import Environment, FileSystemLoader
 import textwrap
+from pathlib import Path
 
 import linkml_antora_generator.schema as Schema
-from linkml_antora_generator.generator import AntoraDocsGenerator, cim_class_color
+from linkml_antora_generator.generator import AntoraDocsGenerator
+
+
+def cim_class_color(class_: ClassDefinition) -> str:
+    package = str(urlparse(class_.from_schema).fragment)
+    top_level_package = ".".join(package.split(".")[:2])
+
+    return {
+        "TC57CIM.IEC61970": "rgb(236, 207, 203)",
+        "TC57CIM.IEC61968": "rgb(209, 231, 194)",
+        "TC57CIM.IEC62325": "rgb(255, 251, 239)",
+    }[top_level_package]
 
 
 if __name__ == "__main__":
-    schema_file = "data/im_capaciteitskaart-full.yaml"
+    data_dir = Path("data")
 
-    with open(schema_file, mode="rb") as f:
-        schema_dict = yaml.safe_load(f)
-        schema = linkml_lang.SchemaDefinition.model_validate(schema_dict)
+    # Single schema.
+    schemas = {Schema.read(data / "im_capaciteitskaart-full.yaml")}
+    adocgen = AntoraDocsGenerator(schemas, class_color=cim_class_color)
 
-    schema = Schema.read(schema_file)
-    adocgen = AntoraDocsGenerator(schema, cim_class_color)
+    # Multiple schemas.
+    schemas = {Schema.read(schema_file) for schema_file in {data / "im_capaciteitskaart-full.yaml", data / "TC57CIM.IEC61970.yaml"}}
+    adocgen = AntoraDocsGenerator(schemas, class_color=cim_class_color, metadata={"name": "nbnl-cim-profile-group", "title": "NBNL CIM Profile Group"})
+
+    # Run.
     adocgen.create_docs()
